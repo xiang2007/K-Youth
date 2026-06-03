f02c8a0233643545bef1d93a4df67330c4462006
Shou Xiang <weeshouxiang@gmail.com>
2026-06-04 00:46:20 +0800

    feat: - Working on ingestor

--- week_1/main.py
+++ week_1/main.py
@@ -1,5 +1,11 @@
+from email.message import EmailMessage
+
 def main():
-    print("Hello from week-1!")
+    res = ""
+    with open("/home/wee/K-Youth/week_1/data/AI & Workflow Automation Associate Job in Kuala Lumpur - Jobstreet.mhtml", "r") as file:
+        for content in file:
+            res = res + content
+    res.
 
 
 if __name__ == "__main__":
---
+++ week_1/src/ingestor.py
@@ -0,0 +1,5 @@
+from bs4 import BeautifulSoup
+from email.message import EmailMessage
+
+def ingest_all_html(input_dir, output_dir) :
+	
