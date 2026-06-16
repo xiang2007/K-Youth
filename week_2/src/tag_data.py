08a970fcee25ffa1f10796157e0bbf2a14d5e409
Shou Xiang <weeshouxiang@gmail.com>
2026-06-16 12:23:50 +0800

    feat: - Refined prompt for both local llm and gemini llm

--- week_2/src/tag_data.py
+++ week_2/src/tag_data.py
@@ -15,63 +15,23 @@ def fetch_next_batch(cursor: sqlite3.Cursor, batch_size: int, offset: int) -> Op
 
     return batch
 
-def split_output(input : str):
-    res = []
-    for line in input.strip().splitlines():
-        if not line:
-            continue
-        source_id, tech_stack = line.split('|', 1)
-        res.append((source_id.strip(), tech_stack.strip()))
-    return res
+def insert_to_db(cursor : sqlite3.Cursor, input):
+    cursor.execute("INSERT INTO jobs (source_id, tech_stack) VALUES (?, ?)", input)
 
 def local_llm(model, job_description):
-    # prompt = f"""You are an advanced technical data extraction tool. Your job is to extract the tech stack from the job description provided below.
-
-    # <job_description>
-    # {job_description}
-    # </job_description>
-
-    # <extraction_checklist>
-    # Scan the text thoroughly for:
-    # - Programming languages
-    # </extraction_checklist>
-
-    # <constraints>
-    # - Extract EVERY explicit technical item mentioned. Do not omit anything.
-    # - Do NOT extract soft skills, administrative processes, or generic categories.
-    # - Do NOT explain or make up technical skills.
-    # - Do Not include terms "no", "bad"
-    # - CRITICAL: If absolutely NO technical stack items from the checklist are found in the text, output exactly the word "None".
-    # </constraints>
-
-    # <formatting_rules>
-    # - Only output one line.
-    # - Output ONLY the final list as a raw, comma-separated string without space (or the single word "None").
-    # - Do not wrap the final output in conversational framing, code blocks, or pleasantries.
-    # - Only return a empty line if nothing is found
-    # - Do NOT give any examples
-    # - If the actual job description text was not given, just return None
-    # - Do not give any notes
-    # - Do not include brackets '()'
-    # - Do not use the word "or", "+'
-    # - Dont use Version control (Git) use Git instead
-    # - Dont include terms "no", "bad"
-    # </formatting_rules>
-
-    # Output:"""
     prompt = f"""Extract the tech stack from each job description.
 
     Rules:
     - Return only technologies.
     - Do not infer technologies.
     - Do not include "Here is the extracted tech stack for each job description:"
-    - Return NONE if no technologies are found.
+    - Output format: ID|NONE if no technologies are found. ID will be the actual id
     - Output format: ID|tech1, tech2, tech3
+    - Do not give any examples
 
     INPUT:{job_description}
 
     Output:"""
-    # res = prompt_model("gemini-2.5-flash-lite", prompt)
     res = prompt_model(model, prompt)
     return res
 
@@ -81,14 +41,13 @@ def gemini_llm(job_description):
     Rules:
     - Return only technologies.
     - Do not infer technologies.
-    - Return NONE if no technologies are found.
-    - Output format: ID|tech1,tech2,tech3
+    - Output format: ID|NONE if no technologies are found.
+    - Output format: ID|tech1, tech2, tech3
 
     INPUT:{job_description}
 
     Output:"""
     res = prompt_model("gemini-2.5-flash-lite", prompt)
-    # res = prompt_model("llama3.1:latest", prompt)
     print(res)
 
 def tag_data(db_url : str):
@@ -109,16 +68,12 @@ def tag_data(db_url : str):
             if not batch:
                 break
             curr_offset += batch_size
-            for item in batch:
-                db_list.append(item)
             # gemini_llm(db_list)
-            res = local_llm("llama3.1:latest", db_list)
-            out.extend(split_output(res.text))
-            db_list.clear()
-        if out:
-            i = 0
-            for item in out:
-                print(f"Analyzed Job{i} : {item[1]}")
-            print(f"Total tokens used: {res.total_tokens}, took {res.time_taken}")
+            res = local_llm("llama3.1:latest", batch)
+            out = res.text.splitlines()
+            for i in out:
+                res = i.split('|', 1)
+                insert_to_db(cs, res)
+                print("Analyzed {res[0]}: res[1]")
         return out
 
